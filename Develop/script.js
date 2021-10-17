1686b0c703a69720176862c2a1658d5b2f6f6dc5
Ben Lu <yudonglv@gmail.com>
2021-10-16 20:37:11 -0700

    new changes

--- Develop/script.js
+++ Develop/script.js
@@ -9,6 +9,7 @@ function generatePassword() {
   var number = "0123456789"
   var special = "!@#$%6&*()"
   var charactersLength = length;
+
   var length = window.prompt("Please enter the length of the password between 8 and 128:");
   if (length > 128 || length < 8)
   {
@@ -19,6 +20,19 @@ function generatePassword() {
   var isLower = confirm ("Is lower case required?");
   var isNumber = confirm("Is number required?");
   var isSpecial = confirm("Is special character required?");
+
+//Object, an object key and value pairs 
+// JSON, javascript object notation, a file that has an array of objects
+
+var optionAnswers = {
+  isUpper: isUpper,
+  isLower: isLower,
+  isNumber: isNumber,
+  isSpecial: isSpecial
+}
+
+return optionAnswers;
+
   if (isUpper== true && isLower==true && isNumber==true && isSpecial==true)
   {
   for ( var i = 0; i < length; i++ ) {
@@ -39,6 +53,35 @@ function generatePassword() {
 }
 
 
+// function that would generates a password 
+function getRandom(arr) {
+  var random = Math.floor(Math.random() * arr.length);
+  var randomElement = arr[random]
+  return randomElement;
+}
+
+// a function to generate password given input
+function generateFinal() {
+  var passwordOptions = generatePassword();
+  var final = []
+  var possibleChar = []
+  var finalChar = []
+
+  // conditional to put together the possible options
+  // conct ['all the numbers', 'all the letters', ''] = possible
+  if(passwordOptions.isNumber) {
+    possibleChar = possibleChar.concat(number);
+    finalChar.push(getRandom(number))
+  };
+
+  if(passwordOptions.isNumber) {
+    possibleChar = possibleChar.concat(number);
+    finalChar.push(getRandom(number))
+  };  
+
+return final.join('')
+
+}
 
 // console.log(generatePassword(length));
 
